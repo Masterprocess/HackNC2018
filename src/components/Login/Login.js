0b9aae34c9faa9adb9a373be954ce0fd1666088c
masterprocess <misterrackley@gmail.com>
2018-10-06 17:55:55 -0400

    added some components

---
+++ src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../Navbar";
+import Wrapper from "../Wrapper";
+import "./Register.css";
+
+const Register = (props) => {   
+  
+  props = props.location.state.props.item;
+
+
+  return (
+    <Wrapper>
+      <Navbar/>
+      <div>Fill out the Registration!</div>
+    </Wrapper>
+  )
+};
+
+export default Register;
---
+++ src/components/Register/Register.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../Navbar";
+import Wrapper from "../Wrapper";
+import "./Register.css";
+
+const Register = (props) => {   
+  
+  props = props.location.state.props.item;
+
+  return (
+    <Wrapper>
+      <Navbar/>
+      <div>Fill out the Registration!</div>
+    </Wrapper>
+  )
+};
+
+export default Register;
