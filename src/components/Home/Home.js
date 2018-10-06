60073e07710e493fdcc3139943941dc3d316a42e
masterprocess <misterrackley@gmail.com>
2018-10-06 19:46:05 -0400

    fixed up landing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import "./Home.css";
 class Home extends Component {
 
 	componentWillMount() {
-		document.title = "Home | Doggers";
+		document.title = "Home | Fetch";
 	}
 
 	render() {
@@ -12,9 +12,10 @@ class Home extends Component {
 			<div>
 			    <div id="titleCenterDiv">
 					<div id="contentDiv">
-						{/* <h1 id="homeTitle">
-							<img id="logo" alt="" src="../assets/"></img>
-						</h1> */}
+					  <h1 id="homeTitle">
+							<img id="logo" alt="dog face!" src="../assets/doggo.png"></img>
+							Fetch
+						</h1>
 						<a href="/signup" className="titleButton" id="signUpButton">Sign Up</a>
 						<a href="/login" className="titleButton" id="logIn">Log In</a>
 						
