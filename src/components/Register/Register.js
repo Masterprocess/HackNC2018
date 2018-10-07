858902bd5a2c4343ccf64f705aa1fdd03ad41698
Patrick Luy <patrickalkuy@gmail.com>
2018-10-06 23:32:43 -0400

    Registration

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "../Navbar";
 import Wrapper from "../Wrapper";
 import "./Register.css";
 
-const Register = (props) => {   
+class Register extends Component {   
   
  //  props = props.location.state.props.item;
 
-  return (
-    <Wrapper>
-      <Navbar/>
-      <div>Fill out the Registration!</div>
-    </Wrapper>
-  )
+  render()
+  {
+  	return (
+  		<form method = "post" action = "">
+    		<input type = "text" name = "name" placeholder = "name"/>
+    		<input type = "email" name = "email" placeholder = "email"/>
+    		<input type = "password" name = "password" placeholder = "password"/>
+    		<input type = "password" name = "confirm_password" placeholder = "Re-Enter Password"/>
+    		<input type = "submit" value = "submit" />
+    	</form>
+  	);
+  }
 };
 
 export default Register;
